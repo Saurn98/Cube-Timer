ce06ef339283d689319e9f8577ebcd8ada2ae0e6
Saurn98 <87410763+Saurn98@users.noreply.github.com>
2022-05-13 22:37:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let timerRef = document.querySelector('.timerDisplay');
+let scoreDisplay = document.querySelector('.scoreDisplay');
+var paused = true;
+let int = null;
+
+document.getElementById('startTimer').addEventListener('click', ()=>{
+    startTimer();
+});
+
+document.getElementById('pauseTimer').addEventListener('click', ()=>{
+    pauseTimer();
+});
+
+document.getElementById('resetTimer').addEventListener('click', ()=>{
+    resetTimer();
+});
+
+function displayTimer(){
+    milliseconds+=10;
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+
+            }
+        }
+    }
+
+ let h = hours < 10 ? "0" + hours : hours;
+ let m = minutes < 10 ? "0" + minutes : minutes;
+ let s = seconds < 10 ? "0" + seconds : seconds;
+ let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+
+ timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+}
+
+function startTimer(){
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+    paused = false;
+}
+
+function pauseTimer(){
+    clearInterval(int);
+    paused = true;
+}
+
+function resetTimer(){
+    clearInterval(int);
+    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+    paused = true;
+}
+
+window.addEventListener("keydown", function(e) {
+    if (paused == true)
+    {
+        startTimer();
+    } else 
+    {
+        pauseTimer();
+        x = document.getElementById("h").innerHTML;
+        console.log(x);
+        scoreDisplay.style.display = "block";
+        scoreDisplay.innerHTML = '<center><p class="score">You solved your cube in'+ x+'</p></center>';
+        resetTimer();
+    }
+});
